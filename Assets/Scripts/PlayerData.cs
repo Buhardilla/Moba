a7db2e916eec636ae06ee36173ee5c52c50fd2a3
Unknown <ica36@alu.ua.es>
2021-03-06 18:23:22 +0100

    Borrado de funciones sin usar

--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -8,34 +8,6 @@ public class PlayerData : MonoBehaviour
 
     public bool hidden = false;
     public int bushes = -1;
-
     public GameObject Minimapicon;
 
-    void Start()
-    {
-        origen = transform.position;
-
-    }
-    public Vector3 final;
-    public float velocidad;
-    private Vector3 origen;
-    private bool direction = true;
-    private Vector3 aux;
-    // Update is called once per frame
-    void Update()
-    {
-        transform.position = Vector3.Lerp(transform.position ,final,Time.deltaTime * velocidad);            
-        if(direction && transform.position.x > (final.x - 1)){
-            direction = !direction;
-            aux = final;
-            final = origen;
-            origen = aux;
-        }
-        if(!direction && transform.position.x < (final.x + 2)){
-            direction = !direction;
-            aux = final;
-            final = origen;
-            origen = aux;
-        }
-    }
 }
