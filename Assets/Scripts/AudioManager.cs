5585453babdb65c5b988244fe01a8ef7caeb5fbf
Javier <javi.m.segura@gmail.com>
2021-03-11 20:48:01 +0100

    AudioManager prefab and sounds folder

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    public static AudioManager instance;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null) //If currently there's no Audio Manager being used
+            instance = this; //Then this one will be the current one
+        else //If there's one already
+        {
+            Destroy(gameObject); //Then it will be destroyed
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach(Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    private void Start()
+    {
+        Play("shakke_1");
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if(s == null) //Sound was not found within the sounds array
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        s.source.Play();
+    }
+
+    public void StopPlaying(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null) //Sound was not found within the sounds array
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        s.source.Stop();
+    }
+}
