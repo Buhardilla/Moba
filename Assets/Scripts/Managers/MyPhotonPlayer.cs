c1f4d5c0baf8858ffbf96cc3f78a6f759240940b
Unknown <Mediotaku@gmail.com>
2021-03-12 17:42:52 +0100

    Online Testing

---
+++ Assets/Scripts/Managers/MyPhotonPlayer.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Realtime;
+using Photon.Pun;
+using System.IO;
+
+public class MyPhotonPlayer : MonoBehaviour
+{
+    PhotonView myPV;
+    GameObject myPlayerAvatar;
+
+    GameObject store;
+
+    GameObject cameraplayer;
+
+    Player[] allPlayers;
+    int myNumberInRoom;
+
+    void Awake()
+    {
+        store.GetComponent<Store>().Player = gameObject;
+        cameraplayer.GetComponent<LookAtPlayer>().player = gameObject; 
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myPV = GetComponent<PhotonView>();
+
+        allPlayers = PhotonNetwork.PlayerList;
+
+        foreach(Player p in allPlayers)
+        {
+            if(p!= PhotonNetwork.LocalPlayer)
+            {
+                myNumberInRoom++;
+            }
+        }
+
+
+
+        if (myPV.IsMine)
+        {
+            myPlayerAvatar = PhotonNetwork.Instantiate(Path.Combine("Prefab","Player"), SpawnManager.instance.spawnPoints[myNumberInRoom].position, Quaternion.identity);
+            
+            if(myNumberInRoom%2==0){
+                gameObject.transform.SetParent(GameObject.Find("Player Dummy Ally").transform);
+            }
+            else
+            {
+                gameObject.transform.SetParent(GameObject.Find("Player Dummy Enemy").transform);
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Managers/SpawnManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public static SpawnManager instance;
+    public Transform[] spawnPoints;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+}
